40af4f66fe88ba83e2e6dbaa3d2f3fb7d005948a
Joseph Adeosun <mdpadeosun@yahoo.ie>
2024-08-29 23:05:01 +0100

    Added some new files for assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true)
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = ()=> {
+    setIsOpen((prev)=>!prev);
+  };
+
+  const toggleTheme = () => {
+    setDarkMode((prev)=>!prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className={`w-full h-full min-h-[100vh] 
+      bg-white ${darkMode && "dark"}`}
+    >
+      <div className="w-full h-full min-h-[100vh] bg-white">
+        <div className="dark:bg-gradient-to-tr from-[#0235a3] via-[#030a1c] to-[#05174e]">
+          <Navbar darkMode={darkMode} isOpen={isOpen}
+
+            toggleMenu={toggleMenu}
+            toggleTheme={toggleTheme}
+          />
+        </div>
+      </div>
     </div>
   );
 }
