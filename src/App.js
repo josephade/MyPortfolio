6ba02d6989f4ed8ad3a697f9bde8a910bcd27f4a
Joseph Adeosun <mdpadeosun@yahoo.ie>
2024-09-10 21:43:53 +0100

    About Me Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import About from "./components/About";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true)
@@ -41,7 +42,14 @@ function App() {
             className="className='w-full bg-[#061130] py-20"
           >
             <Projects />
-          </section>
+        </section>
+
+        <section
+          id='about'
+          className='w-full px-0 lg:px-5 2xl:px-40 py-10 lg:py-0 dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#030a1c] to-[#05174e]'
+        >
+          <About />
+        </section>
 
       </div>
     </div>
